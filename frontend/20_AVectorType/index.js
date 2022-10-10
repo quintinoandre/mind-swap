45aefab682e4673de1a5caf1442e0faf5402b152
André Quintino <andre.quintino@mindswap.academy>
2022-10-10 17:18:41 +0100

    exercise 20

---
+++ frontend/20_AVectorType/index.js
@@ -0,0 +1,25 @@
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(vec) {
+    return new Vec(this.x + vec.x, this.y + vec.y);
+  }
+
+  minus(vec) {
+    return new Vec(this.x - vec.x, this.y - vec.y);
+  }
+
+  get length() {
+    return Math.sqrt(this.x ** 2 + this.y ** 2);
+  }
+}
+
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// → Vec{x: 3, y: 5}
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// → Vec{x: -1, y: -1}
+console.log(new Vec(3, 4).length);
+// → 5
