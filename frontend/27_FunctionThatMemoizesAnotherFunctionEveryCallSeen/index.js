4cf3ae44018d6a90671c2a25afa352e402ffbddc
André Quintino <andre.quintino@mindswap.academy>
2022-11-23 23:24:30 +0000

    refactor: exercise 27

--- frontend/27_FunctionThatMemoizesAnotherFunctionEveryCallSeen/index.js
+++ frontend/27_FunctionThatMemoizesAnotherFunctionEveryCallSeen/index.js
@@ -1,14 +1,12 @@
-const memoize = (func) => {
-	const results = {};
+const memoize = (fn) => {
+	const cache = {};
 
 	return (...args) => {
-		const argsKey = JSON.stringify(args);
-
-		if (!results[argsKey]) {
-			results[argsKey] = func(...args);
+		if (!cache.hasOwnProperty(args.toString())) {
+			cache[args.toString()] = fn(...args);
 		}
 
-		return results[argsKey];
+		return cache[args.toString()];
 	};
 };
 
@@ -32,28 +30,13 @@ console.time('Fifth call');
 console.log(sum(1, 2));
 console.timeEnd('Fifth call');
 
-/* const fibonacci = (n) => {
-	if (n === 1) {
-		return 1;
-	}
-
-	if (n === 2) {
-		return 1;
-	}
-
-	return fibonacci(n - 1) + fibonacci(n - 2);
-}; */
-
 const fibonacci = memoize((n) => {
-	if (n === 1) {
-		return 1;
-	}
-
-	if (n === 2) {
-		return 1;
-	}
-
-	return fibonacci(n - 1) + fibonacci(n - 2);
+	return n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2);
 });
 
+console.time('First call');
 console.log(fibonacci(40));
+console.timeEnd('First call');
+console.time('Second call');
+console.log(fibonacci(40));
+console.timeEnd('Second call');
